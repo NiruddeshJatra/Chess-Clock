d185b4b1ffcf7d0ed317bfa467d87296ec295826
NiruddeshJatra <nasifulalam1212gmail.com>
2024-06-27 22:57:49 +0600

    improved

--- Stats_Window.py
+++ Stats_Window.py
@@ -45,7 +45,10 @@ class ChessStatisticsBar(QWidget):
         if width1:
             painter.drawText(self.xPos + 105, self.yPos + 45, f"{self.causes[0]} {self.percentage1:.2f}%")
         if width2:
-            painter.drawText(self.xPos + width1 + 130, self.yPos + 45, f"{self.causes[1]} {self.percentage2:.2f}%")
+            if width1 and width1 < 110:
+                painter.drawText(self.xPos + width1 + 200, self.yPos + 45, f"{self.causes[1]} {self.percentage2:.2f}%")
+            else:
+                painter.drawText(self.xPos + width1 + 110, self.yPos + 45, f"{self.causes[1]} {self.percentage2:.2f}%")
         if width3:
             painter.drawText(self.xPos + 460, self.yPos + 45, f"{self.causes[2]} {self.percentage3:.2f}%")
         
